8c13fd072dfd027b84fa3b8c1d47bb84d87f039c
Olafi-MooN <alefmastertutor@gmail.com>
2022-10-26 18:16:47 -0300

    update location migrations

--- src/database/query.ts
+++ src/database/query.ts
@@ -1,5 +1,6 @@
 import { db_mysql } from "../config/database/my-sql.config";
 
 db_mysql.connect();
-db_mysql.query('ALTER TABLE Cases CHANGE COLUMN users_id users_id VARCHAR(36);');
+// db_mysql.query('ALTER TABLE Cases CHANGE COLUMN users_id users_id VARCHAR(36);');
+db_mysql.query('ALTER TABLE Users ADD password VARCHAR(255) NOT NULL;');
 db_mysql.end();
